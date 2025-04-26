0883c51d27452d55fd6fd38b223ad1b76a0fd347
Eduardolans <141261251+Eduardolans@users.noreply.github.com>
2025-04-26 18:18:39 +0200

    make required changes for deployment

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,61 @@
+// // import createServer from '@vendia/serverless-express';
+// // import proxy from '@vendia/serverless-express';
+// // import { AppModule } from '../dist/src/app.module';
+// // import { NestFactory } from '@nestjs/core';
+
+// // let server: any;
+
+// // async function bootstrap() {
+// //   const app = await NestFactory.create(AppModule);
+// //   await app.init();
+// //   return createServer(app.getHttpAdapter().getInstance());
+// // }
+
+// // export const handler = async (event: any, context: any) => {
+// //   if (!server) {
+// //     server = await bootstrap();
+// //   }
+// //   return proxy({ server, event, context, promise: 'PROMISE' } as any);
+// // };
+
 // import createServer from '@vendia/serverless-express';
 // import proxy from '@vendia/serverless-express';
-// import { AppModule } from '../dist/src/app.module';
+// import { AppModule } from '../src/app.module'; // Importa SIEMPRE desde src, nunca dist
 // import { NestFactory } from '@nestjs/core';
+// import { AllExceptionsFilter } from '../src/common/exception.filter';
+// import compression from 'compression';
+// import { ValidationPipe } from '@nestjs/common';
+// import helmet from 'helmet';
 
 // let server: any;
 
 // async function bootstrap() {
 //   const app = await NestFactory.create(AppModule);
+
+//   app.use(compression());
+
+//   app.useGlobalPipes(
+//     new ValidationPipe({
+//       whitelist: true,
+//       forbidNonWhitelisted: true,
+//       transform: true,
+//       transformOptions: {
+//         enableImplicitConversion: true,
+//       },
+//     }),
+//   );
+
+//   app.enableCors({
+//     origin: '*',
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     preflightContinue: false,
+//     optionsSuccessStatus: 204,
+//     credentials: true,
+//   });
+//   app.use(helmet());
+//   app.setGlobalPrefix('api/v1');
+//   app.useGlobalFilters(new AllExceptionsFilter());
+
 //   await app.init();
 //   return createServer(app.getHttpAdapter().getInstance());
 // }
@@ -18,51 +67,49 @@
 //   return proxy({ server, event, context, promise: 'PROMISE' } as any);
 // };
 
-import createServer from '@vendia/serverless-express';
-import proxy from '@vendia/serverless-express';
-import { AppModule } from '../src/app.module'; // Importa SIEMPRE desde src, nunca dist
 import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
 import { AllExceptionsFilter } from '../src/common/exception.filter';
-import compression from 'compression';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
+import compression from 'compression';
 
-let server: any;
+let app;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  if (!app) {
+    app = await NestFactory.create(AppModule);
 
-  app.use(compression());
+    app.use(compression());
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    );
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-      transformOptions: {
-        enableImplicitConversion: true,
-      },
-    }),
-  );
+    app.enableCors({
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+      credentials: true,
+    });
+    app.use(helmet());
+    app.setGlobalPrefix('api/v1');
+    app.useGlobalFilters(new AllExceptionsFilter());
 
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-  });
-  app.use(helmet());
-  app.setGlobalPrefix('api/v1');
-  app.useGlobalFilters(new AllExceptionsFilter());
-
-  await app.init();
-  return createServer(app.getHttpAdapter().getInstance());
+    await app.init();
+  }
+  return app;
 }
 
-export const handler = async (event: any, context: any) => {
-  if (!server) {
-    server = await bootstrap();
-  }
-  return proxy({ server, event, context, promise: 'PROMISE' } as any);
-};
+export default async function handler(request, response) {
+  const app = await bootstrap();
+  const instance = app.getHttpAdapter().getInstance();
+  return instance(request, response);
+}
